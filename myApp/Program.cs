98a0efcd4e4274de9b2b422401ecdce5adf328cd
ktw@spyro-soft.com <ktw@spyro-soft.com>
2023-05-10 16:35:37 +0200

    Add super cool method

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -9,6 +9,8 @@
             Console.WriteLine("Siema!");
             Console.WriteLine("Hi!");
 
+            // Some super fun method everyone wants()
+
             // method 1()
             // method2()
             // method3()
